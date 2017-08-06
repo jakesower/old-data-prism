43e26302cea3ccfc2ea3235c0f7fdca91a502d23
Jake Sower <jakesower@gmail.com>
2017-08-06 11:51:11 -0400

    more rollup stuff... starting to get into babel country

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-// import { rollup } from 'rollup';
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import globals from 'rollup-plugin-node-globals';
