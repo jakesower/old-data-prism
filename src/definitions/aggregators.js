f6d5795c121ee1e28f447d157e363c79bb5a31ea
Jake Sower <jsower@collegeboard.org>
2017-08-16 15:35:20 -0400

    add aggs

--- src/definitions/aggregators.js
+++ src/definitions/aggregators.js
@@ -11,11 +11,103 @@ const Count = {
 
   slots: [],
 
-  fn: (group, args) => group.records.length,
+  fn: (group, inputs) => group.records.length,
   // display: (us, cs, dataset) => `<span class="column-name">${dataset.headers[cs.date]}</span> with format ${us.format}`
   display: () => 'Count'
 };
 
+
+const Mean = {
+  name: "Mean",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Column"
+    },
+  ],
+  fn: (group, inputs) => R.mean(inputs.a),
+  display: () => 'Mean'
+};
+
+
+const Median = {
+  name: "Median",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Column"
+    },
+  ],
+  fn: (group, inputs) => R.median(inputs.a),
+  display: () => 'Median'
+};
+
+
+const Maximum = {
+  name: "Maximum",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Column"
+    },
+  ],
+  fn: (group, inputs) => R.max(inputs.a),
+  display: () => 'Maximum'
+};
+
+
+const Minimum = {
+  name: "Minimum",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Column"
+    },
+  ],
+  fn: (group, inputs) => R.min(inputs.a),
+  display: () => 'minimum'
+};
+
+
+const Sum = {
+  name: "Sum",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Column"
+    },
+  ],
+  fn: (group, inputs) => R.sum(inputs.a),
+  display: () => 'Sum'
+};
+
+
+const Product = {
+  name: "Product",
+  slots: [
+    { sourceType: "column",
+      dataType: dataTypes.Number,
+      key: "a",
+      display: "Product"
+    },
+  ],
+  fn: (group, inputs) => R.product(inputs.a),
+  display: () => 'Product'
+};
+
+
+
 module.exports = withKeys({
   Count,
+  Mean,
+  Median,
+  Maximum,
+  Minimum,
+  Sum,
+  Product,
 });
