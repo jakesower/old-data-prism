f4b61b6b32d026843645e1fba5f26ad7b6a10af4
Jake Sower <jsower@collegeboard.org>
2017-08-21 16:51:56 -0400

    ds

---
+++ src/components/multiselect.js
@@ -0,0 +1,42 @@
+const R = require('ramda');
+const h = require('snabbdom/h').default;
+
+module.exports = (options, currentValues, events) => {
+  const optionVdom = option => {
+    const on = R.contains(option.val, currentValues);
+    const nextVal = on ?
+      R.filter(R.complement(R.equals(option.val)), currentValues) :
+      R.append(option.val, currentValues);
+
+    return h('div', {
+      class: {option: true},
+      on: {click: () => events.change(nextVal)}
+    }, [
+      h('div', {class: {check: true}}, on ? '✓' : ''),
+      option.display
+    ])
+  }
+
+  const displayVal = R.pipe(
+    v => R.find(R.propEq('val', v), options),
+    R.prop('display')
+  )
+
+
+  const placeholder = currentValues.length > 0 ?
+    R.pipe(R.map(displayVal), R.join(', '))(currentValues) :
+    'Please select';
+
+
+  return h('div', {class: {'multiselect': true}, attrs: {tabindex: 0}},
+    h('div',
+      {class: {'multiselect-content': true}},
+      [
+        h('div', {class: {placeholder: true}}, placeholder),
+        h('div', {class: {options: true}},
+          R.map(optionVdom, options)
+        )
+      ]
+    )
+  )
+}
