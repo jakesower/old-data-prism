7b31303d3a8fe08a34af3924e88367ce384f8ea8
Jake Sower <jsower@collegeboard.org>
2017-10-16 15:51:20 -0400

    meager

--- src/definitions/joins.js
+++ src/definitions/joins.js
@@ -8,6 +8,7 @@ const Column = require('../types/column');
 
 const JoinCollector = require('../components/collectors/join-collector');
 const {populateSlots, validateSlots} = require('../lib/definition-utils');
+const {join} = require('../lib/relational-functions');
 
 const col = R.curry((dataset, cName) =>
   h('span', {class: {"column-name": true}}, dataset.headers[cName])
@@ -20,21 +21,7 @@ const InnerJoin = {
   tags: ['join'],
   fn: ({sources, dataset: local}, inputs) => {
     const foreign = R.find(s => s.id === inputs.foreignSource, sources);
-    const fKeyed = R.groupBy(R.nth(inputs.foreignKey), foreign);
-
-    return R.reduce(
-      (acc, l) => {
-        const lk = R.nth(inputs.localKey, l);
-        const fks = fKeyed[lk];
-        const rows = fks ?
-          R.chain(f => R.concat(l, f), fks) :
-          [];
-
-        return R.concat(acc, rows);
-      },
-      [],
-      local
-    );
+    return join(local, foreign, inputs.localKey, inputs.foreignKey, R.always([]));
   },
   display: () => 'Inner Join',
   valid: ({sources}, inputs) => R.none(R.isNil, R.values(inputs))
@@ -46,31 +33,33 @@ const LeftOuterJoin = {
   collector: JoinCollector,
   tags: ['join'],
   fn: ({sources, dataset: local}, inputs) => {
-    const foreignSource = R.find(s => s.id === inputs.foreignSource, sources);
-    const foreign = foreignSource.dataset;
+    const foreign = R.find(s => s.id === inputs.foreignSource, sources);
     const emptyForeign = R.map(R.always(''), R.range(0, foreign.headers));
-    const fKeyed = R.groupBy(R.nth(inputs.foreignKey), foreign.records);
 
-    return R.reduce(
-      (acc, l) => {
-        const lk = R.nth(inputs.localKey, l);
-        const fks = fKeyed[lk];
-        const rows = fks ?
-          R.chain(f => R.concat(l, f), fks) :
-          R.concat(l, emptyForeign);
-
-        return R.concat(acc, rows);
-      },
-      [],
-      local
-    );
+    return join(local, foreign, inputs.localKey, inputs.foreignKey, R.concat(R.__, emptyForeign));
   },
   display: () => 'Left Outer Join',
   valid: ({sources}, inputs) => R.none(R.isNil, R.values(inputs))
 }
 
 
+const RightOuterJoin = {
+  name: 'Left Outer Join',
+  collector: JoinCollector,
+  tags: ['join'],
+  fn: ({sources, dataset: local}, inputs) => {
+    const foreign = R.find(s => s.id === inputs.foreignSource, sources);
+    const emptyForeign = R.map(R.always(''), R.range(0, foreign.headers));
+
+    return join(foreign, local, inputs.foreignKey, inputs.localKey, R.concat(R.__, emptyForeign));
+  },
+  display: () => 'Right Outer Join',
+  valid: ({sources}, inputs) => R.none(R.isNil, R.values(inputs))
+}
+
+
 module.exports = {
   InnerJoin,
-  LeftOuterJoin
+  LeftOuterJoin,
+  RightOuterJoin,
 };
